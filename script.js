2d0860709f50d5f4c40748b303efe463f4d9e16e
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-09-05 21:38:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const wrapper = document.querySelector(".wrapper"),
    form = wrapper.querySelector("form"),
    fileInp = form.querySelector("input");
 
+function fetchRequest(formData) {
+   fetch("https://api.qrserver.com/v1/read-qr-code");
+}
+
 fileInp.addEventListener("change", (e) => {
-   let file = e.target.files[0];
-   console.log(file);
+   let file = e.target.files[0]; // Getting User Selected File
+   let formData = new FormData(); // Creating a New FormData Obj
+   formData.append("file", file); // Sffing Select File to FormData
+   fetchRequest(formData);
 });
 
 form.addEventListener("click", () => fileInp.click());
