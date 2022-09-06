65dd3f849051eaefcc34ef0abdaa94299bf8e592
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-09-06 22:02:19 +0200

    Update JS and css

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const wrapper = document.querySelector(".wrapper"),
    form = wrapper.querySelector("form"),
    fileInp = form.querySelector("input");
+infoText = form.querySelector("p");
 
-function fetchRequest(formData) {
-   fetch("https://api.qrserver.com/v1/read-qr-code");
+function fetchRequest(formData, file) {
+   infoText.innerText = "Scanning QR Code...";
+   // Sending Post Request To QR Server API With Passing
+   // Form Data as Body and Getting Response From it
+   fetch("https://api.qrserver.com/v1/read-qr-code/", {
+      method: "POST",
+      body: formData,
+   })
+      .then((res) => res.json())
+      .then((result) => {
+         result = result[0].symbol[0].data;
+         wrapper.querySelector("textarea").innerText = result;
+         form.querySelector("img").src = URL.createObjectURL(file);
+         infoText.innerText = "Upload QR Code to Scan";
+         wrapper.classList.add("active");
+      });
 }
 
 fileInp.addEventListener("change", (e) => {
    let file = e.target.files[0]; // Getting User Selected File
    let formData = new FormData(); // Creating a New FormData Obj
    formData.append("file", file); // Sffing Select File to FormData
-   fetchRequest(formData);
+   fetchRequest(formData, file);
 });
 
 form.addEventListener("click", () => fileInp.click());
